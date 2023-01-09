be17cf661104c078bb8ac198f5fc036376dccb23
Acochisse <1819@holbertonschool.com>
2023-01-09 14:31:44 -0600

    adding modules

---
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import './header.css';
+
+$('body').append(`<div id="logo"></div>`);
+$('body').append(`<p>Holberton Dashboard</p>`);
+console.log('Init header');
