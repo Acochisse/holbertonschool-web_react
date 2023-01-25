c13e5587499b9f947c8201ac95b15be8a2fb6c3c
Acochisse <1819@holbertonschool.com>
2023-01-25 12:22:21 -0600

    did a do

--- 0x01-React_Immutable/5-merge.js
+++ 0x01-React_Immutable/5-merge.js
@@ -1,15 +1,19 @@
 // Create a function named concatElements
-import  {List, Map}  from   'immutable' ;
+import  Immutable  from   'immutable' ;
 
 function concatElements(page1, page2) {
-  return List.concat(page1, page2);
+  const list1 = Immutable.List(page1)
+  const list2 = Immutable.list(page2)
+  return list1.concat(list2)
 }
 
 // Create a function named mergeElements using Map from immutable
 //if one of the values pairs is the same, use the values of page2
 
 function mergeElements(page1, page2) {
-  return Map.merge(page1, page2);
+  const map1 = Immutable.Map(page1)
+  const map2 = Immutable.Map(page2)
+  return map1.merge(map2)
 }
 
 module.exports = {concatElements, mergeElements};
