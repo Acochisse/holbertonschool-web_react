b61925ee87351aa0218d59862c1eba213e2a6bd7
Acochisse <1819@holbertonschool.com>
2023-01-25 10:55:41 -0600

    rework

--- 0x01-React_Immutable/5-merge.js
+++ 0x01-React_Immutable/5-merge.js
@@ -1,16 +1,12 @@
 // Create a function named concatElements
 import  Immutable  from   'immutable' ;
 
-const concatElements = (page1, page2) => {
-  const mylist = Immutable.List(page1);
-  const mylist2 = Immutable.List(page2);
-  return mylist.concat(mylist2);
+function concatElements(page1, page2){
+  return List.concat(page1, page2);
 }
 
 function mergeElements(page1, page2){
-  const mylist = Immutable.List(page1);
-  const mylist2 = Immutable.List(page2);
-  return mylist.merge(mylist2);
+  return List.merge(page1, page2);
 }
 
 export {concatElements, mergeElements};
