26be66926fa6004ad24d0ba8e471e0b0549406ab
Acochisse <1819@holbertonschool.com>
2023-01-25 12:30:43 -0600

    Map>List

--- 0x01-React_Immutable/6-deeply.js
+++ 0x01-React_Immutable/6-deeply.js
@@ -1,8 +1,8 @@
 import Immutable from 'immutable';
 
 function mergeDeeplyElements(page1, page2){
-  const mylist = Immutable.List(page1);
-  const mylist2 = Immutable.List(page2);
+  const mylist = Immutable.Map(page1);
+  const mylist2 = Immutable.Map(page2);
   return mylist.mergeDeep(mylist2);
 }
 
