a9dcaad6c1fae818ea1a82ccbeb4a3c81db1a07c
Acochisse <1819@holbertonschool.com>
2023-01-25 11:07:09 -0600

    attempt n-1

--- 0x01-React_Immutable/5-merge.js
+++ 0x01-React_Immutable/5-merge.js
@@ -1,12 +1,15 @@
 // Create a function named concatElements
 import  {List, Map}  from   'immutable' ;
 
-function concatElements(page1, page2){
+function concatElements(page1, page2) {
   return List.concat(page1, page2);
 }
 
-function mergeElements(page1, page2){
+// Create a function named mergeElements using Map from immutable
+//if one of the values pairs is the same, use the values of page2
+
+function mergeElements(page1, page2) {
   return Map.merge(page1, page2);
 }
 
-export {concatElements, mergeElements};
+module.exports = {concatElements, mergeElements};
